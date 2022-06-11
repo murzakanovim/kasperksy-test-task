3889bba3b82c2c5fb35a50e76c7edde18c3fac55
Ислам <murzakanovislam21@gmail.com>
2022-06-11 16:18:02 +0300

    Init commit

--- Kaspersky/main.cpp
+++ Kaspersky/main.cpp
@@ -1,8 +1,27 @@
 #include <filesystem>
 #include <iostream>
+#include "main.h"
+
+
+namespace fs = std::filesystem;
+
+fs::path translateFromGmiToHtml(fs::path p) {
+  return p;
+}
+
+void generateSite(fs::path input_directory, fs::path output_directory) {
+
+}
+
+void goInDirectory(fs::path path) {
+  for (auto& p : fs::recursive_directory_iterator(path)) {
+  
+  }
+}
 
 int main() {
-  namespace fs = std::filesystem;
-  fs::directory_iterator a;
-  std::cout << fs::current_path();
+  fs::path input_directory;
+  fs::path output_directory;
+  std::cin >> input_directory >> output_directory;
+  generateSite(input_directory, output_directory);
 }
