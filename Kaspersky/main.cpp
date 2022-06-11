4a6a620286fcfdd75246ccc12ede948776de3f87
Ислам <murzakanovislam21@gmail.com>
2022-06-11 17:46:25 +0300

    Add coping non gmi files and folders

--- Kaspersky/main.cpp
+++ Kaspersky/main.cpp
@@ -1,27 +1,51 @@
 #include <filesystem>
 #include <iostream>
-#include "main.h"
-
 
 namespace fs = std::filesystem;
 
-fs::path translateFromGmiToHtml(fs::path p) {
-  return p;
-}
-
-void generateSite(fs::path input_directory, fs::path output_directory) {
-
-}
-
-void goInDirectory(fs::path path) {
-  for (auto& p : fs::recursive_directory_iterator(path)) {
+void translateFromGmiToHtml(std::ifstream& gmi_file)
+{
   
+}
+
+void generateSite(const fs::path& input_directory, const fs::path& output_directory)
+{
+  try
+  {
+    for (const auto& dirEntry : fs::recursive_directory_iterator(input_directory))
+    {
+      const auto& path = dirEntry.path();
+      const auto relativeSrc = fs::relative(path, input_directory);
+      const auto targetParentPath = output_directory / relativeSrc;
+
+      if (dirEntry.is_directory()) {  
+        fs::create_directories(targetParentPath);
+      }
+      else {
+        if (path.extension() == ".gmi") {
+          translateFromGmiToHtml();
+        } 
+        else
+        {
+          fs::copy(path, targetParentPath, fs::copy_options::overwrite_existing);
+        }
+      }
+    }
+  }
+  catch (const std::exception& a)
+  {
+    std::cerr << a.what();
   }
 }
 
 int main() {
-  fs::path input_directory;
-  fs::path output_directory;
-  std::cin >> input_directory >> output_directory;
+  
+  fs::path input_directory(fs::current_path() / "InputDirectory");
+  //fs::path test = fs::relative(input_directory / "Images", input_directory);
+  //std::cout << test;
+  fs::path output_directory(fs::current_path() / "OutputDirectory");
+  //std::cin >> input_directory;
+  //std::cin >> output_directory;
+  
   generateSite(input_directory, output_directory);
 }
